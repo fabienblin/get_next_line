519b5bd375c9c27251e5db6d9e57771540bb6027
fablin <fablin@student.le-101.fr>
2017-12-10 19:17:50 +0100

    wip

--- test.sh
+++
@@ -1,3 +0,0 @@
-make -C ./rendu/libft/
-gcc -Wall -Wextra -Werror ./main.c rendu/get_next_line.c ./rendu/libft/src/*.c -I rendu/ -I rendu/libft/inc rendu/libft/libft.a -o ./test_gnl
-./test_gnl $1 $2 
